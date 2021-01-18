ac84d86a6faa94ead42805653ed8deb466313148
Aishwarya Lingayat <lingayataishwarya16@gmail.com>
2021-01-18 22:19:07 +0530

    initial

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -5,5 +5,7 @@ urlpatterns=[
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
+    path('updateItem/', views.updateItem, name="updateItem"),
+    path('process_order/', views.processOrder, name="process_order"),
 
 ]
